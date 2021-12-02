c13ee13d722383488332620e2dd49e5521cf4128
NiteshBhanji <bhanjinitesh@gmail.com>
2021-12-02 19:45:06 +0530

    Initial Commit

---
+++ credential.php
@@ -0,0 +1,6 @@
+<?php 
+	/*Update credentials*/
+	define('NAME', 'Legame Logistics');
+	define('EMAIL', 'info@legamelogistics.com');
+	define('PASS', 'legame@123');
+ ?>
---
+++ index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html>
+	
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Home | Legame Logistics Pvt. Ltd.</title>
+	<link href="css/index.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script type="text/javascript" src="js/index.js"></script>
+</head>
+
+<body>
+	
+	<header id="MobileHeader">
+		<div class="MenuLogoContact">
+			<div onclick="openNav()" class="OpenMenu">&#9776;</div>
+			<div class="MobileMainLogo"><a href="#"><img src="img/logo.png"></a></div>
+			<div class="NavCall"><a href="tel:8484049531"><i class="fa fa-phone" aria-hidden="true" ></i> Call Us</a></div>
+		</div>
+		
+		<div class="HiddenMenu" id="NavLinks">
+			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+			<div class="NavLinksContent">
+				<a href="index.php">line</a>
+				<a href="#">Services</a>
+				<a href="about.php">About Us</a>
+				<a href="contact.php">Contact</a>
+			</div>
+		</div>
+	</header>
+	
+	<header id="DesktopHeader">
+		<div class="SocialContact">
+			<div class="HeaderContact">
+				<p>Email ID - info@legamelogistics.com</p>
+				<p>Phone No. - 8484049531</p>
+			</div>
+			<div class="HeaderSocialMedia">
+				<a href="#"><img src="img/icons/facebook.png"></a>
+				<a href="#"><img src="img/icons/instagram.png"></a>
+				<a href="#"><img src="img/icons/twitter.png"></a>
+				<a href="https://api.whatsapp.com/send?phone=918484049531"><img src="img/icons/whatsapp.png"></a>
+			</div>
+		</div>
+		<div class="LogoMenu">
+			<div class="DesktopMainLogoContainer"><a href="#"><img src="img/logo.png" id="DesktopMainLogo"></a></div>
+			<nav class="NavMenu">
+				<ul>
+					<li><a href="index.php">line</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	
+	
+	<div class="MainContainer">
+		
+		<div class="Section1MobileBanner">
+			<img src="img/logo.png" class="Sec1Logo">
+			<h3>Make logistics more reliable with our best <a href="#" id="serviceshighlight">services</a></h3>
+			<a href="contact.php#mailform" class="Sec1Btn">Connect With Us</a>
+			<a href="services.php" class="Sec1Link"><p>Click here for more...</p></a>
+			<img src="img/graphic/boat.png" class="Sec1Boat">
+		</div>
+		
+		<div class="Section1DesktopBanner">
+			
+			<img src="img/logo.png" class="Sec1DesktopLogo">
+			<div class="h4heading"><h4>Make logistics more reliable with our best <a href="#" id="serviceshighlight">services</a></h4></div>
+			<div class="pinfo"><p>We are international logistics network presents significant commercial opportunities to our customers, ensure us to identify cost and time saving opportunities through our complete range of services.</p></div>
+			<div class="Sec1DeskBtn"><a href="#" class="Sec1DesktopBtn">Connect With Us</a></div>
+			
+			<img src="img/graphic/Sun.png" id="sun">
+			<img src="img/graphic/Truck.png" id="truck1">
+			<img src="img/graphic/Truck.png" id="truck2">
+			<img src="img/graphic/Bridge.png" id="bridge">
+			<img src="img/graphic/Plane.png" id="plane">
+			<img src="img/graphic/FrontMountain.png" id="mountain">
+			<img src="img/graphic/Boat 1.png" id="boat1">
+			<img src="img/graphic/Boat 2.png" id="boat2">
+			<img src="img/graphic/WaterWaves.png" id="water">
+			
+			
+			
+			<img src="img/graphic/cloud.png" id="cloud1">
+			<img src="img/graphic/cloud.png" id="cloud2">
+			<img src="img/graphic/cloud.png" id="cloud3">
+			<img src="img/graphic/cloud.png" id="cloud4">
+			<img src="img/graphic/cloud.png" id="cloud5">
+			<img src="img/graphic/cloud.png" id="cloud6">
+			
+		</div>
+		
+		<div class="Section2">
+			
+			<div class="Sec2heading"><h2>Our Advantages</h2></div>
+			
+			<div class="UpperSVGWave">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#e10210" fill-opacity="1" d="M0,192L30,197.3C60,203,120,213,180,186.7C240,160,300,96,360,101.3C420,107,480,181,540,197.3C600,213,660,171,720,138.7C780,107,840,85,900,112C960,139,1020,213,1080,224C1140,235,1200,181,1260,160C1320,139,1380,149,1410,154.7L1440,160L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg>
+			</div>
+			
+			<ul>
+				<li><span><img src="img/graphic/conveyor.png"></span><h3>CUSTOM-BUILT<br>END-TO-END<br>TRADE SOLUTIONS</h3></li>
+				<li><span><img src="img/graphic/logistics.png"></span><h3>APPROPRIATE<br>SERVICES ON<br>LATEST TECHNOLOGY</h3></li>
+				<li><span><img src="img/graphic/sale.png"></span><h3>SERVICES AT<br>BEST COMPETITIVE<br>PRICING</h3></li>
+				<li><span><img src="img/graphic/value.png"></span><h3>VALUED-ADDED<br>LOGISTICS</h3></li>
+			</ul>
+			
+			<div class="LowerSVGWave">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e10210" fill-opacity="1" d="M0,192L30,197.3C60,203,120,213,180,186.7C240,160,300,96,360,101.3C420,107,480,181,540,197.3C600,213,660,171,720,138.7C780,107,840,85,900,112C960,139,1020,213,1080,224C1140,235,1200,181,1260,160C1320,139,1380,149,1410,154.7L1440,160L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
+			</div>
+		</div>
+		
+		<!-- <div class="Section3">
+			
+			<div class="Sec3StartSVG">
+				
+				<h3>We are good in </h3>
+				<h1>Our Services</h1>
+			</div>
+			
+			<div class="ServiceContainerWrapper">
+				
+				<div class="ServiceContainer1">
+					<div class="BlackCover">
+					</div>
+					<span class="ServiceContainerSpan1">
+					<h2>NVOCC Operations</h2>
+					<a href="#" class="ServicesRedirectLink">Know More...</a>
+					</span>
+				</div>
+				
+				<div class="ServiceContainer2">
+					<div class="BlackCover"></div>
+					<span class="ServiceContainerSpan2">
+					<h2>Break Bulk Services</h2>
+					<a href="#" class="ServicesRedirectLink">Know More...</a>
+					</span>
+				</div>
+				
+				<div class="ServiceContainer3">
+					<div class="BlackCover"></div>
+					<span class="ServiceContainerSpan3">
+					<h2>Inventory Warehousing</h2>
+					<a href="#" class="ServicesRedirectLink">Know More...</a></span>
+				</div>
+			</div>
+			
+			<div class="Sec3EndSVG">
+				<h1>Our Services</h1>
+			</div>
+			
+		</div> -->
+		
+		<div class="Enquiry-Strip">
+			<span>For more enquiry write mail to us</span>
+			<input type="text" name="Enquiry" placeholder="Enter Email-ID here...">
+			<input type="button" value="Send" name="Enquiry-Btn">
+		</div>
+	</div>
+	
+	
+	<footer id="DesktopFooter">
+		<div class="contact">
+			<img src="img/icons/call.png"><h6>8484049531</h6>
+			<img src="img/icons/mail.png"><h6>info@legamelogistics.in</h6>
+		</div>
+		<h6>LEGAME LOGISTICS PVT. LTD. &#169; ALL RIGHTS RESERVED</h6>
+		<h6>MADE WITH 	&#10084; BY NEPSTARD</h6>
+	</footer>
+	
+	<footer id="MobFooter">
+		<div class="contact">
+			<div><img src="img/icons/call.png"><span>CALL US<br></span>8484049531</div>
+			<div><img src="img/icons/mail.png"><span>EMAIL US<br></span>info@legamelogistics.in</div>
+		</div>
+		<p>LEGAME LOGISTICS PVT. LTD. &#169; ALL RIGHTS RESERVED</p>
+		<p>MADE WITH LOVE BY NEPSTARD</p>
+	</footer>
+	
+	<script>
+			function openNav() {
+  				document.getElementById("NavLinks").style.width = "100%";
+			}
+
+			function closeNav() {
+  				document.getElementById("NavLinks").style.width = "0%";
+			}
+		</script>
+		
+</body>
+	
+</html>
